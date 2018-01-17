d145cda66ae5d8ed74390bbe394e20b9abff9ea0
CursainL <cursain@icloud.com>
2018-01-17 10:52:46 +0800

    Add geo decode function

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -20,10 +20,6 @@ $(window).scroll(function() {
   }
 });
 
-$(document).ready(function(e) {
-        $('img[usemap]').rwdImageMaps();
-});
-
 $('#beitou').on('click', function(){
     layer.open({
       type: 1,
@@ -143,3 +139,94 @@ $('#wenshan').on('click', function(){
     content: '<img class="img-fluid" src="../img/info-wenshan.png">'
   });
 });
+
+
+
+function geolocation(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(geo_success, geo_error, geo_options);
+    } else{
+        console.log('Geolocation is not support by this browser');
+    }
+}
+function geo_success(position){
+    var crd = position.coords;
+    console.log('latitude: ' + crd.latitude);
+    console.log('longitude: ' + crd.longitude);
+    getReverseGeocodingData(crd.latitude, crd.longitude);
+
+}
+function geo_error(err){
+    console.warn('ERROE(' + err.code + '): ' + err.message)
+}
+var geo_options = {
+    enableHighAccuracy: true,
+    maxmumAge:60000,
+    timeout:10000
+};
+
+
+function getReverseGeocodingData(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    // This is making the Geocode request
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status !== google.maps.GeocoderStatus.OK) {
+            alert(status);
+        }
+        // This is checking to see if the Geoeode Status is OK before proceeding
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            var address = (results[0].formatted_address);
+            var userArea = (results[4]["address_components"][0]["short_name"])
+            console.log(userArea);
+            jumpToArea(userArea);
+        }
+    });
+}
+
+function jumpToArea(areaName){
+  alert("您的位置在"+areaName);
+  switch (areaName) {
+    case "大安區":
+      document.location.href="./html/daan.html"
+      break;
+    case "中正區":
+      document.location.href="./html/zhongzheng.html"
+      break;
+    case "大同區":
+      document.location.href="./html/datong.html"
+      break;
+    case "中山區":
+      document.location.href="./html/zhongshan.html"
+      break;
+    case "松山區":
+      document.location.href="./html/songshan.html"
+      break;
+    case "萬華區":
+      document.location.href="./html/wanhua.html"
+      break;
+    case "信義區":
+      document.location.href="./html/xinyi.html"
+      break;
+    case "士林區":
+      document.location.href="./html/shilin.html"
+      break;
+    case "北投區":
+      document.location.href="./html/beitou.html"
+      break;
+    case "內湖區":
+      document.location.href="./html/neihu.html"
+      break;
+    case "南港區":
+      document.location.href="./html/nangang.html"
+      break;
+    case "文山區":
+      document.location.href="./html/wenshan.html"
+      break;
+    default:
+      alert("無法判斷您的所在區域或不在台北市內！")
+      break;
+
+  }
+}
