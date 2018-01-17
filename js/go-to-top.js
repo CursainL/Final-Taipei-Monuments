20cc918c256e0007d29c65871f0385e914f39636
CursainL <cursain@icloud.com>
2018-01-17 08:57:13 +0800

    Add animation, add image map

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -19,3 +19,127 @@ $(window).scroll(function() {
     $(".nav-customize").removeClass('--not-top');
   }
 });
+
+$(document).ready(function(e) {
+        $('img[usemap]').rwdImageMaps();
+});
+
+$('#beitou').on('click', function(){
+    layer.open({
+      type: 1,
+      shadeClose: true,
+      area: ['60vmin', '80vmin'],
+      title:false,
+      content: '<img class="img-fluid" src="../img/info-beitou.png">'
+    });
+  });
+
+$('#shilin').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-shilin.png">'
+  });
+});
+
+$('#neihu').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-neihu.png">'
+  });
+});
+
+$('#datong').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-datong.png">'
+  });
+});
+
+$('#zhongshan').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-zhongshan.png">'
+  });
+});
+
+$('#songshan').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-songshan.png">'
+  });
+});
+
+$('#wanhua').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-wanhua.png">'
+  });
+});
+
+$('#zhongzheng').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-zhongzheng.png">'
+  });
+});
+
+$('#daan').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-daan.png">'
+  });
+});
+
+$('#xinyi').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-xinyi.png">'
+  });
+});
+
+$('#nangang').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-nangang.png">'
+  });
+});
+
+$('#wenshan').on('click', function(){
+  layer.open({
+    type: 1,
+    shadeClose: true,
+    area: ['60vmin', '80vmin'],
+    title:false,
+    content: '<img class="img-fluid" src="../img/info-wenshan.png">'
+  });
+});
